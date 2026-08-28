1829d7300c45ece0b8213ca63b6c9f8fe01227a1
Emaan Asif <emaan.asifsheikh2004@gmail.com>
2026-08-28 18:37:40 +0500

    Deploying

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
         "http://localhost:5175",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://teamsyncc.netlify.app",
         "http://127.0.0.1:5175",
     ],
     allow_credentials=True,
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
+//   timeout: 15000,
+// })
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
   timeout: 15000,
 })
 
